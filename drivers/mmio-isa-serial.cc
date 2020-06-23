fa5ffa6f728755432cf9cee02f0277fa399b2443
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-06-23 14:21:43 -0400

    Merged master

--- drivers/mmio-isa-serial.cc
+++ drivers/mmio-isa-serial.cc
@@ -12,11 +12,11 @@ namespace console {
 mmioaddr_t mmio_isa_serial_console::_addr_mmio;
 u64 mmio_isa_serial_console::_phys_mmio_address;
 
-std::function<u8 (const int&)> isa_serial_console_base::read_byte = [](const int& reg) {
+u8 isa_serial_console_base::read_byte(int reg) {
     return mmio_getb(mmio_isa_serial_console::_addr_mmio + reg);
 };
 
-std::function<void (const u8&, const int&)> isa_serial_console_base::write_byte = [](const u8& val, const int& reg) {
+void isa_serial_console_base::write_byte(u8 val, int reg) {
     mmio_setb(mmio_isa_serial_console::_addr_mmio + reg, val);
 };
 
@@ -25,11 +25,7 @@ void mmio_isa_serial_console::early_init(u64 mmio_phys_address)
     _phys_mmio_address = mmio_phys_address;
     _addr_mmio = reinterpret_cast<char*>(mmio_phys_address);
 
-    common_early_init( [](const int& reg) {
-        return mmio_getb(_addr_mmio + reg);
-    }, [](const u8& val, const int& reg) {
-        mmio_setb(_addr_mmio + reg, val);
-    });
+    common_early_init();
 }
 
 void mmio_isa_serial_console::memory_map()
