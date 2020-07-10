ef97cf271e4ff17de56c96ad29b861bddb7eff00
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-07-10 15:32:56 -0400

    Made tests build for aarch64

---
+++ conf/aarch64-apps.mk
@@ -0,0 +1,34 @@
+ifeq ($(arch),aarch64)
+  #makefile_path_dir = $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
+  #osv_root = $(makefile_path_dir)/..
+  osv_root = /home/wkozaczuk/projects/osv
+
+  aarch64_gccbase = $(osv_root)/build/downloaded_packages/aarch64/gcc/install
+
+  gcc-inc-base := $(dir $(shell find $(aarch64_gccbase)/ -name vector | grep -v -e debug/vector$$ -e profile/vector$$ -e experimental/vector$$))
+  gcc-inc-base2 := $(dir $(shell find $(aarch64_gccbase)/ -name unwind.h))
+  gcc-inc-base3 := $(dir $(shell dirname `find $(aarch64_gccbase)/ -name c++config.h | grep -v /32/`))
+
+  aarch64_cxx_includes = -isystem $(gcc-inc-base)
+  aarch64_cxx_includes += -isystem $(gcc-inc-base3)
+  aarch64_gcc_includes = -isystem $(gcc-inc-base2)
+
+  aarch64_includes = $(aarch64_cxx_includes) $(aarch64_gcc_includes) -isystem $(osv_root)/include/api 
+  aarch64_includes += -isystem $(osv_root)/build/release.aarch64/gen/include
+
+  aarch64_libdir := $(dir $(shell find $(aarch64_gccbase)/ -name libstdc++.so))
+  
+  aarch64_boostbase = $(osv_root)/build/downloaded_packages/aarch64/boost/install
+  boost-includes = -isystem $(aarch64_boostbase)/usr/include
+
+  #CROSS_COMMON_FLAGS = $(aarch64_includes) $(boost-includes) --sysroot $(aarch64_gccbase) -nostdinc
+  CROSS_COMMON_FLAGS = -isystem $(osv_root)/bsd/sys -isystem $(osv_root)/bsd -isystem $(osv_root)/bsd/aarch64 $(boost-includes) --sysroot $(aarch64_gccbase) -nostdinc
+  CROSS_LDFLAGS = -L$(aarch64_libdir)
+
+  CROSS_PREFIX = aarch64-linux-gnu-
+  CXX=$(CROSS_PREFIX)g++
+  CC=$(CROSS_PREFIX)gcc
+  LD=$(CROSS_PREFIX)ld.bfd
+  OBJCOPY=$(CROSS_PREFIX)objcopy
+  export STRIP=$(CROSS_PREFIX)strip
+endif
