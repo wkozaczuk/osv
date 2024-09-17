a71b78192f50c7f9a4391b0f79ed2ce9b8af5059
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2024-09-17 00:14:19 -0400

    TODO: For now comment out usage of std::regex

--- include/osv/tracecontrol.hh
+++ include/osv/tracecontrol.hh
@@ -3,7 +3,7 @@
 
 #include <string>
 #include <vector>
-#include <regex>
+//#include <regex>
 
 class tracepoint_base;
 
@@ -33,14 +33,14 @@ get_event_info();
 event_info
 get_event_info(const ext_id &);
 
-std::vector<event_info>
-get_event_info(const std::regex &);
+//std::vector<event_info>
+//get_event_info(const std::regex &);
 
 event_info
 set_event_state(const ext_id &, bool enable, bool stacktrace = false);
 
-std::vector<event_info>
-set_event_state(const std::regex &, bool enable, bool stacktrace = false);
+//std::vector<event_info>
+//set_event_state(const std::regex &, bool enable, bool stacktrace = false);
 
 event_info
 set_event_state(tracepoint_base &, bool enable, bool stacktrace = false);
