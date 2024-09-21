2b7b3bb0c16e0b04504ed372ab99854058334375
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2024-09-21 10:36:55 -0400

    core/commands.so: use fopen/flose/getline to enable runscript

--- core/commands.cc
+++ core/commands.cc
@@ -104,10 +104,6 @@ struct commands : qi::grammar<sciter,
 std::vector<std::vector<std::string> >
 parse_command_line_min(const std::string line, bool &ok)
 {
-    //TODO
-    //std::vector<std::vector<std::string> > result = { { strdup(line.c_str()), "" } };
-    //std::vector<std::vector<std::string> > result = { { "/httpserver.so", "" } };
-    //ok = true;
     std::vector<std::vector<std::string> > result;
 
     // Lines with only {blank char or ;} are ignored.
@@ -171,11 +167,10 @@ Applying all options before running any command is also safer than trying to
 apply options for each script at script execution (second script would modify
 environment setup by the first script, causing a race).
 */
-/*TODO
 static void runscript_process_options_usage(std::string &message) {
-    std::cout << message << "\n";
-    std::cout << "OSv runscript options:\n";
-    std::cout << "  --env=arg             set Unix-like environment variable (putenv())\n";
+    printf("%s\n", message.c_str());
+    printf("OSv runscript options:\n"
+           "  --env=arg             set Unix-like environment variable (putenv())\n");
     osv::poweroff();
 }
 
@@ -222,7 +217,7 @@ static void runscript_process_options(std::vector<std::vector<std::string> >& re
             if (key.length() > 0) {
                 // we have something to set
                 expand_environ_vars(value);
-                debug("Setting in environment: %s=%s\n", key, value);
+                debugff("Setting in environment: %s=%s\n", key, value);
                 setenv(key.c_str(), value.c_str(), 1);
             }
         }
@@ -230,7 +225,7 @@ static void runscript_process_options(std::vector<std::vector<std::string> >& re
         cmd.erase(cmd.begin(), cmd.begin() + nr_options);
         result[ii] = cmd;
     }
-}*/
+}
 
 /*
 If cmd starts with "runscript file", read content of file and
@@ -240,16 +235,15 @@ ok flag is set to false on parse error, and left unchanged otherwise.
 
 If cmd doesn't start with runscript, then vector with size 0 is returned.
 */
-/* TODO
 std::vector<std::vector<std::string>>
 runscript_expand(const std::vector<std::string>& cmd, bool &ok, bool &is_runscript)
 {
     std::vector<std::vector<std::string> > result2, result3;
     if (cmd[0] == "runscript") {
         is_runscript = true;
-        //
-        //The cmd vector ends with additional ";" or "\0" element.
-        //
+        /*
+        The cmd vector ends with additional ";" or "\0" element.
+        */
         if (cmd.size() != 3 && cmd[2].c_str()[0] != 0x00) {
             puts("Failed expanding runscript - filename missing or extra parameters present.");
             ok = false;
@@ -257,23 +251,28 @@ runscript_expand(const std::vector<std::string>& cmd, bool &ok, bool &is_runscri
         }
         auto fn = cmd[1];
 
-        std::ifstream in(fn);
-        if (!in.good()) {
+        FILE *fp = fopen(fn.c_str(), "r");
+        if (!fp) {
             printf("Failed to open runscript file '%s'.\n", fn.c_str());
             ok = false;
             return result2;
         }
-        std::string line;
-        size_t line_num = 0;
-        while (!in.eof()) {
-            getline(in, line);
+        size_t line_num = 0, line_length = 80;
+        char *line_buffer = (char*)malloc(line_length);
+        ssize_t read;
+        while ((read = getline(&line_buffer, &line_length, fp)) != -1) {
+            if (read && line_buffer[read - 1] == '\n') {
+                line_buffer[read - 1] = 0; //Remove new line character
+            }
             bool ok2;
-            result3 = parse_command_line_min(line, ok2);
-            printf("runscript expand fn='%s' line=%d '%s'\n", fn.c_str(), line_num, line.c_str());
+            result3 = parse_command_line_min(line_buffer, ok2);
+            printf("runscript expand fn='%s' line=%d '%s'\n", fn.c_str(), line_num, line_buffer);
             if (ok2 == false) {
-                printf("Failed expanding runscript file='%s' line=%d '%s'.\n", fn.c_str(), line_num, line.c_str());
+                printf("Failed expanding runscript file='%s' line=%d '%s'.\n", fn.c_str(), line_num, strlen(line_buffer));
                 result2.clear();
                 ok = false;
+                free(line_buffer);
+                fclose(fp);
                 return result2;
             }
             // Replace env vars found inside script.
@@ -287,17 +286,19 @@ runscript_expand(const std::vector<std::string>& cmd, bool &ok, bool &is_runscri
             result2.insert(result2.end(), result3.begin(), result3.end());
             line_num++;
         }
+        free(line_buffer);
+        fclose(fp);
     }
     else {
         is_runscript = false;
     }
     return result2;
-}*/
+}
 
 std::vector<std::vector<std::string>>
 parse_command_line(const std::string line,  bool &ok)
 {
-    std::vector<std::vector<std::string> > result;//, result2;
+    std::vector<std::vector<std::string> > result, result2;
     result = parse_command_line_min(line, ok);
     // First replace environ variables in input command line.
     expand_environ_vars(result);
@@ -306,7 +307,7 @@ parse_command_line(const std::string line,  bool &ok)
     If command starts with runscript, we need to read actual command to
     execute from the given file.
     */
-    /*TODO
+    //TODO: This should be #ifdef optional
     std::vector<std::vector<std::string>>::iterator cmd_iter;
     for (cmd_iter=result.begin(); ok && cmd_iter!=result.end(); ) {
         bool is_runscript;
@@ -323,7 +324,7 @@ parse_command_line(const std::string line,  bool &ok)
         else {
             cmd_iter++;
         }
-    }*/
+    }
 
     return result;
 }
