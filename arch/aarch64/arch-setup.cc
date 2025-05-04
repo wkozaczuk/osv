5a0b174427908788e69cf2d91bf5803796473d29
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2025-05-04 15:42:13 +0000

    Enable nvme

--- arch/aarch64/arch-setup.cc
+++ arch/aarch64/arch-setup.cc
@@ -197,6 +197,9 @@ void arch_init_premain()
 #if CONF_drivers_virtio_fs
 #include "drivers/virtio-fs.hh"
 #endif
+#if CONF_drivers_nvme
+#include "drivers/nvme.hh"
+#endif
 
 void arch_init_drivers()
 {
@@ -248,6 +251,9 @@ void arch_init_drivers()
 #endif
 #if CONF_drivers_virtio_fs
     drvman->register_driver(virtio::fs::probe);
+#endif
+#if CONF_drivers_nvme
+    drvman->register_driver(nvme::driver::probe);
 #endif
     boot_time.event("drivers probe");
     drvman->load_all();
